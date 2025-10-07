8f47f38f16a117cc02259db2daf6a320bc973f0c
mattia rossi <mattia.rossi@il.com>
2025-10-07 12:04:27 +0200

    Remove API - leave essential tooling

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import createApp from './app';
-import config from './config';
-
-createApp().then((app) => {
-  app.listen(config.port, () => {
-    config.logger.info(`🚀  Server ready at http://0.0.0.0:${config.port}/`);
-    config.logger.info(`Running version: ${config.version}`)
-  });
-});
